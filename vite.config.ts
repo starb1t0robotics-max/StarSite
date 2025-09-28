1d248fcff0d500512357b80c89762bebbcb5853f
Strixx <oargapavel@yahoo.com>
2025-09-28 17:35:29 +0300

    ye2

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Site_b1t0/',
+  base: '/StarSite/',
 })
